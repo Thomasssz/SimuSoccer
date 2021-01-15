d68db0eed5e2e06bf8bcb128a7b628a8547c8938
Thomasssz <thomasledreo@hotmail.fr>
2021-01-15 17:14:23 +0100

    modif MOA

--- src/Moe1.java
+++ src/Moe1.java
@@ -6,6 +6,7 @@ public class Moe1 {
     	
        System.out.println("Classe fait par le moe1");
        System.out.println("Classe fait par le chef");
+       System.out.println("modification faite par le moa");
     }
 	
 }
