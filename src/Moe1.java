3beec28755931b7d2d23f0f8e20cab9141d895bf
Martinho <Martinho@rosaetgomes>
2021-01-15 17:03:20 +0100

    commit de la classe pour le maître d'oeuvre 1

---
+++ src/Moe1.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Moe1 {
+
+	public static void main (String argv []) throws IOException {
+    	
+       System.out.println("Classe fait par le moe1");
+        
+    }
+	
+}
